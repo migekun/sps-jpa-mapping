3d4c8812b4cb2f4d6986665af7d3b76e5a82efdf
mimikun <mi@miU>
2017-11-19 22:06:01 +0100

    Initial commit

---
+++ src/main/java/org/formacio/setmana2/domini/Matricula.java
@@ -0,0 +1,48 @@
+package org.formacio.setmana2.domini;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_matricules")
+public class Matricula {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="mat_id")
+	private Long id; 
+	
+	@ManyToOne	
+	@JoinColumn(name="mat_alumne") 
+	private Alumne alumne;
+	
+	@ManyToOne	
+	@JoinColumn(name="mat_curs")
+	private Curs curs;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Alumne getAlumne() {
+		return alumne;
+	}
+	public void setAlumne(Alumne alumne) {
+		this.alumne = alumne;
+	}
+	public Curs getCurs() {
+		return curs;
+	}
+	public void setCurs(Curs curs) {
+		this.curs = curs;
+	}
+	
+	
+}
---
+++ src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -0,0 +1,36 @@
+package org.formacio.setmana2.repositori;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.formacio.setmana2.domini.Alumne;
+import org.formacio.setmana2.domini.Curs;
+import org.formacio.setmana2.domini.Matricula;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
+ * operacions de persistencia tal com indiquen les firmes dels metodes
+ */
+@Repository
+@Transactional
+public class RepositoriEscola {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public Curs carregaCurs(String nom) {
+		return em.find(Curs.class, nom);
+	}
+	
+	
+	public Matricula apunta (String alumne, String curs) throws EdatIncorrecteException {
+		  Matricula matricula = new Matricula();
+		  matricula.setAlumne(em.find(Alumne.class, alumne));
+		  matricula.setCurs(em.find(Curs.class, curs));
+		  return matricula;
+	}
+	
+	
+}
